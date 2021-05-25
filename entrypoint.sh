8dd02b688a191d95199a7475601876c456373246
Marcus Lindblom <m@marcuslindblom.com>
2021-05-25 12:27:59 +0200

    Remove unused code

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,7 @@
 
 URL=$1
 
-# TASKID=$(curl -L -X POST -H 'Content-Type: application/json' \
-# --data-raw '{
-# 	"address": "'$URL'"
-# }' 'https://analyse.imagekit.io/analysis' | jq -r '.taskId')
-
 check() {
-	# RESULT=$(curl -L -X GET 'https://analyse.imagekit.io/status/'$TASKID'' | jq -r '.state')
 	STATE=$(curl -L -X GET 'https://analyse.imagekit.io/status/1619712183360' | jq -r '.state')
 }
 
@@ -27,9 +21,3 @@ echo "::set-output name=savings::$SAVINGS%"
 echo "::set-output name=protip::$PROTIP"
 
 exit 0
-
-# if [ $SAVINGS -ge $SCORE ]; then
-# 	exit 0
-# else
-# 	exit 1
-# fi
