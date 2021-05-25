5cae355312fc965c79e97d668c1351deb0cf1332
Marcus Lindblom <m@marcuslindblom.com>
2021-05-25 12:41:29 +0200

    Fixes issue with hard coded status 😹

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,13 @@
 
 URL=$1
 
-# TASKID=$(curl -L -X POST -H 'Content-Type: application/json' \
-# --data-raw '{
-# 	"address": "'$URL'"
-# }' 'https://analyse.imagekit.io/analysis' | jq -r '.taskId')
+TASKID=$(curl -L -X POST -H 'Content-Type: application/json' \
+--data-raw '{
+	"address": "'$URL'"
+}' 'https://analyse.imagekit.io/analysis' | jq -r '.taskId')
 
 check() {
-	# RESULT=$(curl -L -X GET 'https://analyse.imagekit.io/status/'$TASKID'' | jq -r '.state')
-	STATE=$(curl -L -X GET 'https://analyse.imagekit.io/status/1619712183360' | jq -r '.state')
+	STATE=$(curl -L -X GET 'https://analyse.imagekit.io/status/'$TASKID'' | jq -r '.state')
 }
 
 check #1st execution
@@ -19,17 +18,12 @@ while [ "$STATE" == "processing" ]; do
 	check
 done
 
-RESULT=$(curl -L -H 'Content-Type: application/json' 'https://analyse.imagekit.io/status/1619712183360')
+RESULT=$(curl -L -H 'Content-Type: application/json' 'https://analyse.imagekit.io/status/'$TASKID'')
 SAVINGS=$(echo $RESULT | jq -r '.report.finalFormattedResults.totalImagePercentSaving')
 PROTIP=$(echo $RESULT | jq -r '.report.finalFormattedResults.proTip')
 
 echo "::set-output name=savings::$SAVINGS%"
 echo "::set-output name=protip::$PROTIP"
+echo "::set-output name=report-url::https://analyse.imagekit.io/status/'$TASKID'"
 
 exit 0
-
-# if [ $SAVINGS -ge $SCORE ]; then
-# 	exit 0
-# else
-# 	exit 1
-# fi
